fcde41654d64bc502d71fb565f8480fff7a5789e
Michael Springer <mike@sprngr.me>
2017-11-02 23:55:23 -0400

    Implemented grid based movement, hoorah

---
+++ grid/grid.gd
@@ -0,0 +1,58 @@
+extends TileMap
+
+var tile_size = get_cell_size()
+var half_tile_size = tile_size / 2
+
+enum ENTITY_TYPES {
+	PLAYER,
+	OBSTACLE,
+	COLLECTIBLE
+}
+
+var grid_size = Vector2(15, 10)
+var grid = []
+
+onready var Obstacle = preload("res://obstacle.tscn")
+
+
+func _ready():
+	for x in range(grid_size.x):
+		grid.append([])
+
+		for y in range(grid_size.y):
+			grid[x].append(null)
+
+	var Player = get_node("Player")
+	var start_pos =  update_child_pos(Player)
+	Player.set_pos(start_pos);
+
+	var positions = []
+	for n in range(5):
+		var grid_pos = Vector2(randi() % int(grid_size.x) - 1, randi() % int(grid_size.y) - 1)
+		if not grid_pos in positions:
+			positions.append(grid_pos)
+
+	for pos in positions:
+		var new_obstacle = Obstacle.instance()
+		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
+		grid[pos.x][pos.y] = OBSTACLE
+		add_child(new_obstacle)
+
+func is_cell_vacant(pos, direction):
+	var grid_pos = world_to_map(pos) + direction
+
+	if grid_pos.x < grid_size.x and grid_pos.x >= 0:
+		if grid_pos.y < grid_size.y and grid_pos.y >= 0:
+			return true if grid[grid_pos.x][grid_pos.y] == null else false
+	return false
+
+func update_child_pos(child_node):
+	var grid_pos = world_to_map(child_node.get_pos())
+	print(grid_pos)
+	grid[grid_pos.x][grid_pos.y] = null
+
+	var new_grid_pos = grid_pos + child_node.direction
+	grid[new_grid_pos.x][new_grid_pos.y] = child_node.type
+
+	var target_pos = map_to_world(new_grid_pos) + half_tile_size
+	return target_pos
---
+++ player/player.gd
@@ -0,0 +1,52 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 64
+
+var grid
+var type
+var speed = 0
+var direction = Vector2()
+var velocity = Vector2()
+
+var is_moving = false
+var target_pos = Vector2()
+var target_direction = Vector2()
+
+func _ready():
+	grid = get_parent()
+	type = grid.PLAYER
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	direction = Vector2()
+
+	if Input.is_action_pressed("move_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("move_down"):
+		direction.y = 1
+	if Input.is_action_pressed("move_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("move_left"):
+		direction = -1
+
+	if not is_moving and direction != Vector2():
+		target_direction = direction
+		if grid.is_cell_vacant(get_pos(), target_direction):
+			target_pos = grid.update_child_pos(self)
+			is_moving = true
+	elif is_moving:
+		speed = MAX_SPEED
+		velocity = speed * target_direction.normalized() * delta
+
+		var pos = get_pos()
+		var distance_to_target = Vector2(abs(target_pos.x - pos.x), abs(target_pos.y - pos.y))
+
+		if abs(velocity.x) > distance_to_target.x:
+			velocity.x = distance_to_target.x * target_direction.x
+			is_moving = false
+
+		if abs(velocity.y) > distance_to_target.y:
+			velocity.y = distance_to_target.y * target_direction.y
+			is_moving = false
+
+		move(velocity)
