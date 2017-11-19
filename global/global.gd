df028a360edf2f914bf5fe8bc0abce81b34f780c
Michael Springer <mike@sprngr.com>
2017-11-19 04:17:14 -0500

    Added font, added menu to title and save function.

--- global/global.gd
+++ global/global.gd
@@ -11,14 +11,46 @@ func get(key):
 	
 func initialize():
 	# TODO Load in save data first before loading default values
-	game_data.player_direction = Vector2()
-	game_data.viewport = Vector2()
-	game_data.state = "GAME_PLAYING"
-	game_data.map = {
-		id = "overworld",
-		spawn = "game_start"
-	}
 	
+	game_data = {
+		player_direction = Vector2(),
+		player_position = Vector2(),
+		viewport = Vector2(),
+		state = "GAME_PLAYING",
+		map = {
+			id = "overworld",
+			spawn = "game_start"
+		}
+	}
+
+func load_save():
+	pass
+	
+func prepare_save():
+	var save_dict = {
+		map = game_data.map.id,
+		player_direction = {
+			x = game_data.player_direction.x,
+			y = game_data.player_direction.y
+		},
+		player_position = {
+			x = game_data.player_position.x,
+			y = game_data.player_position.y
+		},
+		state = game_data.state,
+		viewport = {
+			x = game_data.viewport.x,
+			y = game_data.viewport.y
+		}
+	}
+	return save_dict
+	
+func save_game():
+	var savegame = File.new()
+	savegame.open("user://unplanned_save.save", File.WRITE)
+	savegame.store_line(prepare_save().to_json())
+	savegame.close()
+
 func set_scene(scene):
 	var new_scene = ResourceLoader.load(scene)
 	current_scene.queue_free()
